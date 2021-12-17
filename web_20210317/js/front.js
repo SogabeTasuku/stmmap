341e2c82bec038c50381bbf66d13268ce4c69491
SogabeTasuku <tasuku679@gmail.com>
2021-12-17 14:14:00 +0900

    shoukyo

--- web_20210317/js/front.js
+++
@@ -1,9 +0,0 @@
-function clickCopy1() {
-	
-}
-
-function clickCss(value) {
-	$("#text1").val(value);
-	$("#text2").val("color: " + value + ";");
-	$("#text3").val("background-color: " + value + ";");
-}
