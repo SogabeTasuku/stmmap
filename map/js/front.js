9066978b33288d40f8e120037e71593df6cf2ed2
stm <michi.tsk.3596@gmail.com>
2022-01-22 16:42:50 +0900

    ソース修正

--- map/js/front.js
+++ map/js/front.js
@@ -1,6 +1,12 @@
 window.onload = function() {
 	// ページ読み込み時の処理
-	
+	var MyLatLng = new google.maps.LatLng(35.6811673, 139.7670516);
+	var Options = {
+	zoom: 15,      //地図の縮尺値 tasuku3
+	center: MyLatLng,    //地図の中心座標
+	mapTypeId: 'roadmap'   //地図の種類
+	};
+	var map = new google.maps.Map(document.getElementById('map'), Options);
 }
 
 function clickCopy(id) {
